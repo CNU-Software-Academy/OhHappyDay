8b7a97f42faeb1fbb04f7b317f6a297a4a71ea40
ohgee98 <ohgee98@naver.com>
2023-01-12 14:05:38 +0900

    init

---
+++ src/main/java/cnu/ohd/OhdApplication.java
@@ -0,0 +1,13 @@
+package cnu.ohd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OhdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cnu/ohd/OhdApplicationTests.java
@@ -0,0 +1,13 @@
+package cnu.ohd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
