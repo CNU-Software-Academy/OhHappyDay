d390bb96406f46adf3c8f188f29c8301aa6f0133
ohgee98 <ohgee98@naver.com>
2023-02-27 15:55:35 +0900

    checked api 생성 완료

--- src/main/java/cnu/ohd/gift/repository/GiftRepository.java
+++ src/main/java/cnu/ohd/gift/repository/GiftRepository.java
@@ -1,6 +1,5 @@
 package cnu.ohd.gift.repository;
 
-import cnu.ohd.Member.domain.Member;
 import cnu.ohd.gift.domain.Gift;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +21,13 @@ public class GiftRepository {
         return em.createQuery("select m from Gift m where m.member.id = :memberId", Gift.class)
                 .setParameter("memberId",memberId)
                 .getResultList();
+    }
+
+    public void changeGift(long giftId) {
+        em.createQuery("update Gift m set m.checked = 1 where m.giftId = :giftId")
+                .setParameter("giftId",giftId)
+                .executeUpdate();
+        em.clear();
     }
 
 
